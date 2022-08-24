dc2eb7c738bf6107798afac4fb6865b12503bdc9
timcastillogill <timgillviola@gmail.com>
2022-08-24 14:37:34 +0100

    GREEN: Passing  with 2

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,8 +1,7 @@
 public class StringCalculator {
     public int add(String string) {
-        if (string.equals("1")) {
-            return 1;
-        }
-        return 0;
+        if (string.equals("")) { return 0; }
+
+        return Integer.parseInt(string);
     }
 }
