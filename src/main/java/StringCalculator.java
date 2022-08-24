5f9b4259ffbff1f1f39b37a9c0776ecd1c05b7a4
Tim Turvey <tturvey@hotmail.co.uk>
2022-08-24 15:44:20 +0100

    Green: new line seperator passes

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,11 +1,9 @@
-import java.util.Arrays;
-
 public class StringCalculator {
 
     public int add(String string) {
         if (string.equals("")) { return 0; }
 
-        String[] stringArray = string.split(",");
+        String[] stringArray = string.split("[,\n]");
         int result = 0;
         for (String number : stringArray) {
             result += Integer.parseInt(number);
