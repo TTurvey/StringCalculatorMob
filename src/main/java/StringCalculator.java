d57047a1fc0134bf8c34de7a984455f4189ae58e
Tim Turvey <tturvey@hotmail.co.uk>
2022-08-24 15:10:46 +0100

    Green: 1,2 passes

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,7 +1,16 @@
+import java.util.Arrays;
+
 public class StringCalculator {
     public int add(String string) {
         if (string.equals("")) { return 0; }
 
-        return Integer.parseInt(string);
+        String[] stringArray = string.split(",");
+
+        if (stringArray.length == 1) {
+            return Integer.parseInt(string);
+        }
+
+        return Integer.parseInt(stringArray[0]) + Integer.parseInt(stringArray[1]);
+
     }
 }
