4ed7d0816bbcc9d27594a2cdd311aeeb1196c198
MattW <mattnwarnock@yahoo.co.uk>
2022-08-24 16:32:16 +0100

    Red: bug in string array

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,8 +3,17 @@ public class StringCalculator {
     public int add(String string) {
         if (string.equals("")) { return 0; }
 
-        String[] stringArray = string.split("[,\n]");
         int result = 0;
+        char separator = ',';
+
+        if (string.matches("^//((.|\\n)*)")) {
+            separator = string.charAt(2);
+        }
+
+        System.out.println(separator);
+
+        String[] stringArray = string.split("[" + separator + "\n]");
+
         for (String number : stringArray) {
             result += Integer.parseInt(number);
         }
