759c4b01302a145f7f49217e0a1a775e3fe6a843
timcastillogill <timgillviola@gmail.com>
2022-08-24 15:35:26 +0100

    GREEN: Passing with five numbers

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,22 +1,16 @@
 import java.util.Arrays;
 
 public class StringCalculator {
+
     public int add(String string) {
         if (string.equals("")) { return 0; }
 
         String[] stringArray = string.split(",");
-
-        if (stringArray.length == 2) {
-            return Integer.parseInt(stringArray[0]) +
-                   Integer.parseInt(stringArray[1]);
-        }
-        if (stringArray.length == 3) {
-            return Integer.parseInt(stringArray[0]) +
-                   Integer.parseInt(stringArray[1]) +
-                   Integer.parseInt(stringArray[2]);
+        int result = 0;
+        for (String number : stringArray) {
+            result += Integer.parseInt(number);
         }
 
-
-        return Integer.parseInt(string);
+        return result;
     }
 }
